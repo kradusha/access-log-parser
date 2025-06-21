cbd1541cdef39629ec0622ab6774257f312ee6b8
Nastasya Gavrilova <nastasya1301@yandex.ru>
2025-06-22 01:38:00 +0300

    Добавлен бесконечный цикл для считывания пути до файла

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,18 +1,22 @@
+import java.io.File;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Введите первое число: ");
-        int firstNumber = new Scanner(System.in).nextInt();
-        System.out.println("Введите второе число: ");
-        int secondNumber = new Scanner(System.in).nextInt();
-        int sum = firstNumber + secondNumber;
-        System.out.println("Сумма чисел: " + sum);
-        int diff = firstNumber - secondNumber;
-        System.out.println("Разность чисел: " + diff);
-        int mult = firstNumber * secondNumber;
-        System.out.println("Произведение чисел: " + mult);
-        double quotient = (double) firstNumber / secondNumber;
-        System.out.println("Частное чисел: " + quotient);
+        int count = 0;
+        while (true) {
+            String path = new Scanner(System.in).nextLine();
+            File file = new File(path);
+            boolean fileExists = file.exists();
+            boolean isDirectory = file.isDirectory();
+            if (!fileExists | isDirectory) {
+                System.out.println("Указанный путь ведёт к папке или файл не существует" + fileExists + isDirectory);
+                continue;
+            } else {
+                System.out.println("Путь указан верно");
+                count++;
+                System.out.println("Это файл номер " + count);
+            }
+        }
     }
 }
